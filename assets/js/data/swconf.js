1bd7138570a660818c80481523d795bb62e665db
Das-Alpha <81019679+Das-Alpha@users.noreply.github.com>
2025-04-02 18:42:36 +0200

    Avatar

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1743611134',resources: [
+    cacheName: 'chirpy-1743612136',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
