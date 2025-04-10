9da833d21c4ee1d4b665167e2382bf8ad7b11383
Das-Alpha <81019679+Das-Alpha@users.noreply.github.com>
2025-04-10 12:25:19 +0200

    Building...

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1743928698',resources: [
+    cacheName: 'chirpy-1744280718',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
