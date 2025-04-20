10d541fa052b19d48f6c2816642911ebd8c4e807
Das-Alpha <81019679+Das-Alpha@users.noreply.github.com>
2025-04-20 15:07:54 +0200

    Building...

---
+++ _pages/worlddomination/test.md
@@ -0,0 +1,1 @@
+Naja, klappt schon mal gut!
--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1745154086',resources: [
+    cacheName: 'chirpy-1745154473',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
